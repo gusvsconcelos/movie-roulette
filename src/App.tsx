8f18542097c8acc9ba77324836faea690f859437
Gustavo Vasconcelos <gluiz9445@gmail.com>
2024-07-19 15:46:01 -0300

    todo: fix aligment of elements in the footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
 // import Button from './components/ui/Button';
+// TODO: fix alignment of elements in the footer component (maybe also at header?)
 
 export default function App() {
   return (
