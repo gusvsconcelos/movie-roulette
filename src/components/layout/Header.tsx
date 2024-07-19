99a363f6175710e2e2022cfdcfcab9969ec0f329
Gustavo Vasconcelos <gluiz9445@gmail.com>
2024-07-19 19:36:51 -0300

    change header title design

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,8 +4,8 @@ import Nav from '../ui/Nav';
 
 export default function Header() {
   return (
-    <header className='flex justify-center items-center p-12 bg-white shadow-brute'>
-      <Title styles='absolute left-20 text-4xl text-black' />
+    <header className='flex justify-center items-center p-14 bg-white shadow-brute'>
+      <Title styles='absolute left-20 text-4xl text-black bg-white p-4 rounded-md border-4 border-black drop-shadow-brute' />
       <Nav />
       <Switch />
     </header>
