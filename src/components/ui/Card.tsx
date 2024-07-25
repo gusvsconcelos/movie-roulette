6cec2cf7dfcb3a37773e49b7cc0744d3f3c6ca57
Gustavo Vasconcelos <gluiz9445@gmail.com>
2024-07-25 20:25:43 -0300

    start building the landing page api fetch

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -1,25 +1,46 @@
-export function Card() {
+interface Props {
+  poster?: string;
+  movie?: string;
+  director?: string;
+  gender?: string;
+  date?: string;
+  length?: string;
+}
+
+const defaultProps: Props = {
+  poster: 'src/assets/images/movie-poster-placeholder.jpg',
+  movie: 'Movie Name',
+  director: 'Director',
+  gender: 'Gender',
+  date: '01-01-1970',
+  length: '2h 0m',
+};
+
+export function Card(prop: Props) {
+  const finalProps = { ...defaultProps, ...prop };
+  const { poster, movie, director, gender, date, length } = finalProps;
+
   return (
     <article className='flex justify-center items-center w-fit h-fit m-2 p-1 border-2 border-black bg-yellow-100 shadow-brute'>
-      {/* movie cover */}
+      {/* movie poster */}
       <div className='relative w-56 h-80 m-1'>
-        <div className='absolute top-8 left-4 w-48 h-[269px] border-2 border-black'></div>
+        <div className='absolute top-8 left-4 w-[195px] h-[275px] border-2 border-black'></div>
 
         <img
-          src='src/assets/images/movie-poster-placeholder.jpg'
+          src={poster}
           alt='Poster Placeholder'
-          className='absolute top-5 left-2 w-48'
+          className='absolute top-5 left-2 w-48 h-[275px]'
         />
       </div>
 
       {/* movie information */}
       <div className='w-fit h-80 m-1 py-14 pr-3'>
         <div className='flex-row justify-end items-end mb-4'>
-          <h1 className='font-bolder text-4xl'>Movie Name</h1>
-          <h2 className='font-bolder text-2xl'>Director Name</h2>
-          <h3 className='font-ui text-xl'>Movie Gender</h3>
-          <h3 className='font-ui text-xl'>Movie Year</h3>
-          <h3 className='font-ui text-xl'>Movie Length</h3>
+          <h1 className='font-bolder text-4xl'>{movie}</h1>
+          <h2 className='font-bolder text-2xl'>{director}</h2>
+          <h3 className='font-ui text-xl'>{gender}</h3>
+          <h3 className='font-ui text-xl'>{date}</h3>
+          <h3 className='font-ui text-xl'>{length}</h3>
         </div>
 
         {/* color palettes */}
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,56 @@
+import { useEffect, useState } from 'react';
 import { Card } from '../components/ui/Card';
 
 export function Home() {
-  return <Card />;
+  const [moviePoster, setMoviePoster] = useState();
+  const [movieName, setMovieName] = useState('Movie Name');
+  const [movieDate, setMovieDate] = useState('01-01-1970');
+  let movieID: number;
+
+  useEffect(() => {
+    const apiKey = import.meta.env.API_KEY;
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${apiKey}`,
+      },
+    };
+
+    const fetchData = async () => {
+      const response = await fetch(
+        'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1',
+        options,
+      );
+      const data = await response.json();
+
+      console.log(data);
+
+      setMoviePoster(data.results[3].poster_path);
+      setMovieName(data.results[3].title);
+      setMovieDate(data.results[3].release_date);
+      movieID = data.results[3].id;
+    };
+
+    const fetchCredits = async () => {
+      const response = await fetch(
+        `https://api.themoviedb.org/3/movie/${movieID}/credits?language=en-US`,
+        options,
+      );
+      const data = await response.json();
+
+      console.log(data);
+    };
+
+    fetchCredits();
+    fetchData();
+  }, []);
+
+  return (
+    <Card
+      poster={`https://image.tmdb.org/t/p/original/${moviePoster}`}
+      movie={movieName}
+      date={movieDate}
+    />
+  );
 }
