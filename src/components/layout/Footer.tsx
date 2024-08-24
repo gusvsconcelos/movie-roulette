7a40589c506e2121e91f8962f27c02bf055e286d
Gustavo Vasconcelos <136681497+gusvsconcelos@users.noreply.github.com>
2024-08-24 02:49:43 -0300

    rename project

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -3,8 +3,8 @@ import { Title } from '../ui/Title';
 export function Footer() {
   return (
     <footer className='flex justify-center items-center p-10 bg-black'>
-      <Title styles='absolute left-16 text-yellow-100 text-xl' />
-      <p className='absolute right-16 text-xs font-ui text-yellow-100'>
+      <Title styles='absolute left-16 text-white text-xl' />
+      <p className='absolute right-16 text-xs font-ui text-white'>
         &copy; {new Date().getFullYear()}{' '}
         <a href='https://www.linkedin.com/in/gusvsconcelos' target='_blank'>
           Gustavo Vasconcelos
--- src/components/ui/Title.tsx
+++ src/components/ui/Title.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 export function Title({ styles }: { styles: string }) {
   return (
     <h1 className={`font-title font-black italic ${styles}`}>
-      <Link to='/'>MOVIE SORTER</Link>
+      <Link to='/'>MOVIE ROULETTE</Link>
     </h1>
   );
 }
