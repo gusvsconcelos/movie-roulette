8a47b4fe6210c32c68eea6491dfd8795b8256576
Gustavo Vasconcelos <136681497+gusvsconcelos@users.noreply.github.com>
2024-09-08 16:47:36 -0300

    minor design adjustments

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ export default {
       },
       boxShadow: {
         brute: '6px 6px 0 rgb(35 15 10)',
-        header: '0 4px 0 rgb(35 15 10)',
+        header: '0 6px 0 rgb(35 15 10)',
         button: '4px 4px 0 rgb(35 15 10)',
       },
       fontFamily: {
