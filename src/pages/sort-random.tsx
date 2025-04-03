13fc37bd68ea4b639b0b45338a830aee1035c0ab
Gustavo Vasconcelos <gluiz9445@gmail.com>
2025-04-03 16:51:26 -0300

    start building the random movie page

--- src/pages/sort-random.tsx
+++ src/pages/sort-random.tsx
@@ -1,6 +1,17 @@
 import { RandomMovie } from '../components/ui/Title'
+import { moviesData } from '../utils/data'
 
 export function SortMovie() {
+  const getRandomInt = (max: number) => {
+    return Math.floor(Math.random() * max)
+  }
+
+  const authToken: string = import.meta.env.VITE_ACCESS_AUTH_TOKEN
+  const index: number = getRandomInt(20)
+  const movie = moviesData(index, authToken)
+
+  console.log(movie)
+
   return (
     <>
       <RandomMovie />
