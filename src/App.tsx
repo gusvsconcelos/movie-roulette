469aaeecc24c96d11a5c0b814f4f508c4f502f7a
Gustavo Vasconcelos <gluiz9445@gmail.com>
2025-04-27 16:37:52 -0300

    remove sort from list

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Route, HashRouter as Router, Routes } from 'react-router-dom'
 import { Layout } from './components/layout/Layout'
 import { Home } from './pages/home'
-import { SortList } from './pages/sort-list'
 import { SortMovie } from './pages/sort-random'
 
 export default function App() {
@@ -11,7 +10,6 @@ export default function App() {
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
           <Route path="/sort-random" element={<SortMovie />} />
-          <Route path="/sort-list" element={<SortList />} />
         </Route>
       </Routes>
     </Router>
--- src/components/ui/Nav.tsx
+++ src/components/ui/Nav.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom'
-import { Github, Dices, ListCheck } from 'lucide-react'
+import { Github, Dices } from 'lucide-react'
 
 export function Nav() {
   return (
@@ -11,13 +11,7 @@ export function Nav() {
         <Dices />
         Sort Random Movie
       </Link>
-      <Link
-        className="flex items-center gap-1 mx-4 hover:underline underline-offset-4 decoration-2"
-        to="/sort-list"
-      >
-        <ListCheck />
-        Sort Movie From List
-      </Link>
+
       <a
         className="flex items-center gap-1 w-fit mx-4 p-2 border-2 border-black bg-[#DAA192] hover:shadow-button transition-all"
         href="https://github.com/gusvsconcelos/movie-roulette.git"
