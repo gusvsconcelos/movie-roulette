b8bf3a1348fb1984625bcb347973b75ade997776
Gustavo Vasconcelos <gustavo.vs.me@gmail.com>
2026-03-14 01:40:01 -0300

    feat: add grid layout as background

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -7,7 +7,7 @@ export function Button({ text, onClick }: ButtonProps) {
   return (
     <button
       type="button"
-      className="mt-4 p-3 border-[3px] border-black shadow-button bg-[#DAA192] font-title font-bold text-2xl antialiased  active:shadow-none active:bg-[#AB5139] active:text-[#F8EEEC] transition-all"
+      className="mt-4 p-3 border-[3px] border-black shadow-button bg-[#DAA192] font-title font-bold text-2xl antialiased active:shadow-none active:bg-[#AB5139] active:text-[#F8EEEC] transition-all"
       onClick={onClick}
     >
       {text}
