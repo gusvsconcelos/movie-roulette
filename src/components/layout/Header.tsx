ac6d56ad7d458b9b55986410ae67a9d049143d8a
Gustavo Vasconcelos <gustavo.vs.me@gmail.com>
2026-03-14 01:02:37 -0300

    chore: remove toggleswitch.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,13 +1,11 @@
 import { Nav } from "../ui/Nav";
 import { Title } from "../ui/Title";
-// import { ToggleSwitch } from '../ui/ToggleSwitch'
 
 export function Header() {
   return (
     <header className="flex justify-center items-center p-20 bg-[#F8EEEC] shadow-header select-none">
       <Title styles="absolute left-16 text-7xl text-black" />
       <Nav />
-      {/* <ToggleSwitch /> */}
     </header>
   );
 }
